80df6ef6bb1d3b5f80c65844588c98fa3d15e44d
lucasluchi <lds.lucasluchi@gmail.com>
2020-07-22 09:00:23 -0300

    firts commit

---
+++ jogo-da-adivinhacao/jogodaadivinhacao.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    cout << "*********************************************" << endl;
+    cout << "***** Bem vindos ao jogo da adivinhacao *****" << endl;
+    cout << "*********************************************" << endl;
+
+    int numero_secreto = 42;
+
+    cout << "O numero secreto e " << numero_secreto << ". Nao conte para ninguem" << endl;
+
+
+}
