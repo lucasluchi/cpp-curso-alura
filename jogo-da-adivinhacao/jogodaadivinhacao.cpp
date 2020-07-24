69d9195c75b427a2bdc58635997346c493cacc36
lucasluchi <lds.lucasluchi@gmail.com>
2020-07-24 08:24:12 -0300

    implementação das variáveis condicionais

--- jogo-da-adivinhacao/jogodaadivinhacao.cpp
+++ jogo-da-adivinhacao/jogodaadivinhacao.cpp
@@ -4,12 +4,28 @@ using namespace std;
 
 int main (){
     cout << "*********************************************" << endl;
-    cout << "***** Bem vindos ao jogo da adivinhacao *****" << endl;
+    cout << "***** Bem vindos ao jogo da adivinhação *****" << endl;
     cout << "*********************************************" << endl;
 
-    int numero_secreto = 42;
+    const int numero_secreto = 42;
 
-    cout << "O numero secreto e " << numero_secreto << ". Nao conte para ninguem" << endl;
+    int chute;
+    cout << "Qual seu chute? ";
+    cin >> chute;
 
+    cout << "O valor do seu chute é: " << chute << endl;
 
+    bool acertou = chute == numero_secreto;
+    bool maior = chute > numero_secreto;
+
+    if (acertou){
+        cout << "Parabéns! Você acertou o número secreto!" << endl;
+    }
+    else if (maior){
+        cout << "Seu chute foi maior que o número secreto!" << endl;
+    }
+    else{
+        cout << "Seu chute foi menor que o número secreto!" << endl;
+    }
+    
 }
